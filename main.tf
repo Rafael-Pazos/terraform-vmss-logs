0fef98429996e56cd371e8c41a1db4b98010d410
Rafael Pazos <rarod@microsoft.com>
2022-03-22 10:49:54 +0100

    rework

--- main.tf
+++ main.tf
@@ -34,6 +34,14 @@ resource "azurerm_log_analytics_workspace" "vmss" {
   retention_in_days   = 30
 }
 
+resource "azurerm_application_insights" "vmss" {
+  name                = "vmss-appinsights"
+  location            = azurerm_resource_group.vmss.location
+  resource_group_name = azurerm_resource_group.vmss.name
+  workspace_id        = azurerm_log_analytics_workspace.vmss.id
+  application_type    = "other"
+}
+
 resource "azurerm_security_center_workspace" "vmss" {
   scope        = "/subscriptions/95b44dd6-5808-485e-9f1a-923eaeef3b37"
   workspace_id = azurerm_log_analytics_workspace.vmss.id
@@ -60,6 +68,7 @@ resource "azurerm_network_security_group" "vmss" {
   resource_group_name = azurerm_resource_group.vmss.name
 }
 
+# VMSS
 resource "azurerm_subnet_network_security_group_association" "vmss" {
   subnet_id                 = azurerm_subnet.vmss.id
   network_security_group_id = azurerm_network_security_group.vmss.id
@@ -78,12 +87,10 @@ resource "azurerm_lb" "vmss" {
  name                = "vmss-lb"
  location            = var.location
  resource_group_name = azurerm_resource_group.vmss.name
-
  frontend_ip_configuration {
    name                 = "PublicIPAddress"
    public_ip_address_id = azurerm_public_ip.vmss.id
  }
-
  tags = var.tags
 }
 
@@ -121,41 +128,35 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
    tier     = "Standard"
    capacity = 2
  }
-
  storage_profile_image_reference {
    publisher = "Canonical"
    offer     = "UbuntuServer"
    sku       = "16.04-LTS"
    version   = "latest"
  }
-
  storage_profile_os_disk {
    name              = ""
    caching           = "ReadWrite"
    create_option     = "FromImage"
    managed_disk_type = "Standard_LRS"
  }
-
  storage_profile_data_disk {
    lun          = 0
    caching        = "ReadWrite"
    create_option  = "Empty"
    disk_size_gb   = 10
  }
-
  os_profile {
    computer_name_prefix = "vmlab"
    admin_username       = var.admin_user
    admin_password       = var.admin_password
  }
-
  os_profile_linux_config {
    disable_password_authentication = false
  }
  network_profile {
    name    = "terraformnetworkprofile"
    primary = true
-
    ip_configuration {
      name                                   = "IPConfiguration"
      subnet_id                              = azurerm_subnet.vmss.id
@@ -163,8 +164,20 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
      primary = true
    }
  }
-
  tags = var.tags
+}
+
+resource "azurerm_virtual_machine_scale_set_extension" "bash-ext-vmss" {
+ name                         = "bash-ext-vmss"
+ virtual_machine_scale_set_id = azurerm_virtual_machine_scale_set.vmss.id
+ publisher                    = "Microsoft.Azure.Extensions"
+ type                         = "CustomScript"
+ type_handler_version         = "2.1"
+ protected_settings = <<PROTECTED_SETTINGS
+    {
+      "commandToExecute": "sudo apt-get update && sudo apt-get -y install unzip && sudo apt-get -y install stress-ng"
+    }
+ PROTECTED_SETTINGS 
 }
 
 resource "azurerm_virtual_machine_scale_set_extension" "vmss" {
@@ -196,6 +209,7 @@ resource "azurerm_virtual_machine_scale_set_extension" "vmssdep" {
   auto_upgrade_minor_version = true
 }
 
+# jumpbox VM for testing purposes
 resource "azurerm_public_ip" "jumpbox" {
  name                         = "jumpbox-public-ip"
  location                     = var.location
@@ -209,14 +223,12 @@ resource "azurerm_network_interface" "jumpbox" {
  name                = "jumpbox-nic"
  location            = var.location
  resource_group_name = azurerm_resource_group.vmss.name
-
  ip_configuration {
    name                          = "IPConfiguration"
    subnet_id                     = azurerm_subnet.vmss.id
    private_ip_address_allocation = "dynamic"
    public_ip_address_id          = azurerm_public_ip.jumpbox.id
  }
-
  tags = var.tags
 }
 
@@ -226,34 +238,41 @@ resource "azurerm_virtual_machine" "jumpbox" {
  resource_group_name   = azurerm_resource_group.vmss.name
  network_interface_ids = [azurerm_network_interface.jumpbox.id]
  vm_size               = "Standard_DS1_v2"
-
  storage_image_reference {
    publisher = "Canonical"
    offer     = "UbuntuServer"
    sku       = "16.04-LTS"
    version   = "latest"
  }
-
  storage_os_disk {
    name              = "jumpbox-osdisk"
    caching           = "ReadWrite"
    create_option     = "FromImage"
    managed_disk_type = "Standard_LRS"
  }
-
  os_profile {
    computer_name  = "jumpbox"
    admin_username = var.admin_user
    admin_password = var.admin_password
  }
-
  os_profile_linux_config {
    disable_password_authentication = false
  }
-
  tags = var.tags
 }
 
+resource "azurerm_virtual_machine_extension" "bash-ext-jumpbox" {
+ name                         = "bash-ext-jumpbox"
+ virtual_machine_id           = azurerm_virtual_machine.jumpbox.id
+ publisher                    = "Microsoft.Azure.Extensions"
+ type                         = "CustomScript"
+ type_handler_version         = "2.1"
+ protected_settings = <<PROTECTED_SETTINGS
+    {
+      "commandToExecute": "sudo apt-get update && sudo apt-get -y install unzip && sudo apt-get -y install stress-ng"
+    }
+ PROTECTED_SETTINGS 
+}
 resource "azurerm_virtual_machine_extension" "jumpbox" {
   name                 = "LoganalyticsAgent"
   virtual_machine_id   = azurerm_virtual_machine.jumpbox.id
@@ -288,4 +307,203 @@ resource "azurerm_virtual_machine_extension" "jumpboxnet" {
   type                 = "NetworkWatcherAgentLinux"
   type_handler_version = "1.4"
   auto_upgrade_minor_version = true
+}
+
+#second VM for testing purposes
+
+resource "azurerm_public_ip" "testvm" {
+ name                         = "testvm-public-ip"
+ location                     = var.location
+ resource_group_name          = azurerm_resource_group.vmss.name
+ allocation_method            = "Static"
+ domain_name_label            = "${random_string.fqdn.result}-http"
+ tags                         = var.tags
+}
+
+resource "azurerm_network_interface" "testvm" {
+ name                = "testvm-nic"
+ location            = var.location
+ resource_group_name = azurerm_resource_group.vmss.name
+ ip_configuration {
+   name                          = "IPConfiguration"
+   subnet_id                     = azurerm_subnet.vmss.id
+   private_ip_address_allocation = "dynamic"
+   public_ip_address_id          = azurerm_public_ip.testvm.id
+ }
+ tags = var.tags
+}
+resource "azurerm_virtual_machine" "testvm" {
+ name                  = "testvm"
+ location              = var.location
+ resource_group_name   = azurerm_resource_group.vmss.name
+ network_interface_ids = [azurerm_network_interface.testvm.id]
+ vm_size               = "Standard_DS1_v2"
+ storage_image_reference {
+   publisher = "Canonical"
+   offer     = "UbuntuServer"
+   sku       = "16.04-LTS"
+   version   = "latest"
+ }
+ storage_os_disk {
+   name              = "testvm-osdisk"
+   caching           = "ReadWrite"
+   create_option     = "FromImage"
+   managed_disk_type = "Standard_LRS"
+ }
+ os_profile {
+   computer_name  = "testvm"
+   admin_username = var.admin_user
+   admin_password = var.admin_password
+ }
+ os_profile_linux_config {
+   disable_password_authentication = false
+ }
+ tags = var.tags
+}
+
+resource "azurerm_virtual_machine_extension" "bash-ext-testvm" {
+ name                         = "bash-ext-testvm"
+ virtual_machine_id           = azurerm_virtual_machine.testvm.id
+ publisher                    = "Microsoft.Azure.Extensions"
+ type                         = "CustomScript"
+ type_handler_version         = "2.1"
+ protected_settings = <<PROTECTED_SETTINGS
+    {
+      "commandToExecute": "sudo apt-get update && sudo apt-get -y install unzip && sudo apt-get -y install stress-ng"
+    }
+ PROTECTED_SETTINGS 
+}
+resource "azurerm_virtual_machine_extension" "testvm" {
+  name                 = "LoganalyticsAgent"
+  virtual_machine_id   = azurerm_virtual_machine.testvm.id
+  publisher            = "Microsoft.EnterpriseCloud.Monitoring"
+  type                 = "OmsAgentForLinux"
+  type_handler_version = "1.13"
+  settings = jsonencode(
+    {
+      "workspaceId": azurerm_log_analytics_workspace.vmss.workspace_id
+    }
+  )
+  protected_settings = jsonencode(
+    {
+      "workspaceKey" : azurerm_log_analytics_workspace.vmss.primary_shared_key
+    }
+  )
+}
+
+resource "azurerm_virtual_machine_extension" "testvmdep" {
+  name                 = "DependencysAgent"
+  virtual_machine_id   = azurerm_virtual_machine.testvm.id
+  publisher            = "Microsoft.Azure.Monitoring.DependencyAgent"
+  type                 = "DependencyAgentLinux"
+  type_handler_version = "9.5"
+  auto_upgrade_minor_version = true
+}  
+
+resource "azurerm_virtual_machine_extension" "testvmnet" {
+  name                 = "NetworkWatcherAgent"
+  virtual_machine_id = azurerm_virtual_machine.testvm.id
+  publisher            = "Microsoft.Azure.NetworkWatcher"
+  type                 = "NetworkWatcherAgentLinux"
+  type_handler_version = "1.4"
+  auto_upgrade_minor_version = true
+}
+
+# identity for chaos studio
+resource "azurerm_user_assigned_identity" "chaos-studio" {
+  location                     = var.location
+  resource_group_name          = azurerm_resource_group.vmss.name
+  name = "chaos-studio-user"
+}
+
+# the network watcher
+#resource "azurerm_network_watcher" "vmss" {
+#  name                         = "vmss-Watcher"
+#  location                     = var.location
+#  resource_group_name          = azurerm_resource_group.vmss.name
+#}
+
+# a connection monitor
+resource "azurerm_network_connection_monitor" "jumpbox" {
+  name                         = "jumpbox-internet-Monitor"
+  network_watcher_id           ="/subscriptions/95b44dd6-5808-485e-9f1a-923eaeef3b37/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/NetworkWatcher_westeurope"
+  #network_watcher_id           = azurerm_network_watcher.vmss.id
+  location                     = var.location
+  
+  endpoint {
+    name               = "jumpbox"
+    target_resource_id = azurerm_virtual_machine.jumpbox.id
+  }
+
+  endpoint {
+    name    = "destination"
+    address = "terraform.io"
+  }
+
+  test_configuration {
+    name                      = "tcpName"
+    protocol                  = "Tcp"
+    test_frequency_in_seconds = 60
+
+    tcp_configuration {
+      port = 80
+    }
+  }
+
+  test_group {
+    name                     = "exampletg"
+    destination_endpoints    = ["destination"]
+    source_endpoints         = ["jumpbox"]
+    test_configuration_names = ["tcpName"]
+  }
+
+  notes = "examplenote"
+
+  output_workspace_resource_ids = [azurerm_log_analytics_workspace.vmss.id]
+
+  depends_on = [azurerm_virtual_machine_extension.testvmnet]
+}
+
+# second connection monitor
+resource "azurerm_network_connection_monitor" "jumpbox-test" {
+  name                         = "jumpbox-test-Monitor"
+  network_watcher_id           ="/subscriptions/95b44dd6-5808-485e-9f1a-923eaeef3b37/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/NetworkWatcher_westeurope"
+  location                     = var.location
+ 
+  endpoint {
+    name                       = "jumpbox"
+    target_resource_id         = azurerm_virtual_machine.jumpbox.id
+    target_resource_type       = "AzureVM"
+  }
+  endpoint {
+    name                       = "testvm"
+    target_resource_id         = azurerm_virtual_machine.testvm.id
+    target_resource_type       = "AzureVM"
+  }
+  test_configuration {
+    name                      = "tcpName"
+    protocol                  = "Tcp"
+    test_frequency_in_seconds = 60
+    tcp_configuration {
+      port = 80
+    }
+  }
+  test_configuration {
+    name                      = "ping"
+    protocol                  = "Icmp"
+    test_frequency_in_seconds = 60
+    icmp_configuration {
+      trace_route_enabled = true
+    }
+  }
+  test_group {
+    name                     = "jumpbox-test"
+    destination_endpoints    = ["testvm"]
+    source_endpoints         = ["jumpbox"]
+    test_configuration_names = ["tcpName", "ping"]
+  }
+  notes = "examplenote"
+
+  output_workspace_resource_ids = [azurerm_log_analytics_workspace.vmss.id]
+  depends_on = [azurerm_virtual_machine_extension.testvmnet]
 }
--- output.tf
+++ output.tf
@@ -9,3 +9,19 @@ output "jumpbox_public_ip_fqdn" {
 output "jumpbox_public_ip" {
    value = azurerm_public_ip.jumpbox.ip_address
 }
+
+output "testvm_public_ip_fqdn" {
+   value = azurerm_public_ip.testvm.fqdn
+}
+
+output "testvm_public_ip" {
+   value = azurerm_public_ip.testvm.ip_address
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.vmss.instrumentation_key
+}
+
+output "app_id" {
+  value = azurerm_application_insights.vmss.app_id
+}
